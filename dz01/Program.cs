a845a1d787cae329be73da3c87218976f70047f2
Irena Akhmetova <a.irena337@gmail.com>
2022-11-29 16:44:58 +0300

    Dz 01

---
+++ dz01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input firts number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine($"Maximum number: {a} Minimal number: {b}");
+}
+else
+{
+Console.WriteLine($"Maximum number: {b} Minimal number: {a}");
+}
